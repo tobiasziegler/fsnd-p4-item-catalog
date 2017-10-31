09643f3104724f4ea5b9d1039af5952688f55384
David Mallard <correspondence@davidmallard.id.au>
2017-11-01 07:44:07 +1100

    Fix an indentation error affecting login session data

--- application.py
+++ application.py
@@ -112,7 +112,7 @@ def gconnect():
     user_id = getUserID(login_session['email'])
     if not user_id:
         user_id = createUser(login_session)
-        login_session['user_id'] = user_id
+    login_session['user_id'] = user_id
 
     output = ''
     output += '<h1>Welcome, '
